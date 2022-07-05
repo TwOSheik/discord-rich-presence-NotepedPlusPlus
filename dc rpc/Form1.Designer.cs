2f6f010084d2be74c0bbafa0f3a16ddb55174afb
TwOSheik <108615734+TwOSheik@users.noreply.github.com>
2022-07-05 18:51:50 -0300

    Discord rich presence noteped++
    <center><b>Simples Discord Rich Presence</b></center>
    <br><br>
    <img src="https://i.imgur.com/JtuAve3.png" />
    <br><br>
    <img src="https://i.imgur.com/3grut5W.png" />

---
+++ dc rpc/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace dc_rpc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(134, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(169, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Developing: EXIT CHEAT WEB";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(665, 319);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "index.php";
+            this.textBox2.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(134, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(169, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "992453323896655972";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(134, 64);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(169, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "php";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(134, 90);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(169, 20);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.Text = "vsc";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(134, 116);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(134, 145);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 201);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ dc rpc/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DiscordRPC;
+using DiscordRPC.Logging;
+
+namespace dc_rpc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public struct ProcessEntry32
+        {
+            // Token: 0x04000037 RID: 55
+            public uint dwSize;
+
+            // Token: 0x04000038 RID: 56
+            public uint cntUsage;
+
+            // Token: 0x04000039 RID: 57
+            public uint th32ProcessID;
+
+            // Token: 0x0400003A RID: 58
+            public IntPtr th32DefaultHeapID;
+
+            // Token: 0x0400003B RID: 59
+            public uint th32ModuleID;
+
+            // Token: 0x0400003C RID: 60
+            public uint cntThreads;
+
+            // Token: 0x0400003D RID: 61
+            public uint th32ParentProcessID;
+
+            // Token: 0x0400003E RID: 62
+            public int pcPriClassBase;
+
+            // Token: 0x0400003F RID: 63
+            public uint dwFlags;
+
+            // Token: 0x04000040 RID: 64
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+            public string szExeFile;
+        }
+        string procPID;
+        // Token: 0x0600002A RID: 42
+        [DllImport("KERNEL32.DLL", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        public static extern IntPtr CreateToolhelp32Snapshot(uint flags, uint processid);
+
+        // Token: 0x0600002B RID: 43
+        [DllImport("KERNEL32.DLL", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        public static extern int Process32First(IntPtr handle, ref ProcessEntry32 pe);
+
+        // Token: 0x0600002C RID: 44
+        [DllImport("KERNEL32.DLL", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        public static extern int Process32Next(IntPtr handle, ref ProcessEntry32 pe);
+        public string GetProcID(int index)
+        {
+            string result = "";
+            checked
+            {
+                if (index == 1 || index == 0)
+                {
+                    IntPtr intPtr = IntPtr.Zero;
+                    uint num = 0U;
+                    IntPtr intPtr2 = CreateToolhelp32Snapshot(2U, 0U);
+                    if ((int)intPtr2 > 0)
+                    {
+                        ProcessEntry32 processEntry = default(ProcessEntry32);
+                        processEntry.dwSize = (uint)Marshal.SizeOf<ProcessEntry32>(processEntry);
+                        for (int num2 = Process32First(intPtr2, ref processEntry); num2 == 1; num2 = Process32Next(intPtr2, ref processEntry))
+                        {
+                            IntPtr intPtr3 = Marshal.AllocHGlobal((int)processEntry.dwSize);
+                            Marshal.StructureToPtr<ProcessEntry32>(processEntry, intPtr3, true);
+                            object obj = Marshal.PtrToStructure(intPtr3, typeof(ProcessEntry32));
+                            ProcessEntry32 processEntry2 = (obj != null) ? ((ProcessEntry32)obj) : default(ProcessEntry32);
+                            Marshal.FreeHGlobal(intPtr3);
+
+                            if (processEntry2.szExeFile.Contains("notepad++") && processEntry2.cntThreads > num)
+                            {
+                                num = processEntry2.cntThreads;
+                                intPtr = (IntPtr)((long)(unchecked((ulong)processEntry2.th32ProcessID)));
+                            }
+                        }
+                    }
+                    result = Convert.ToString(intPtr);
+                    procPID = Convert.ToString(intPtr);
+                }
+                return result;
+            }
+
+
+        }
+
+        string phpact = "";
+        public DiscordRpcClient client;
+        bool initalized = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            initalized = true;
+            client = new DiscordRpcClient($"{textBox3.Text}");
+            client.Logger = new ConsoleLogger() { Level = LogLevel.Warning };
+            client.Initialize();
+            update();
+            timer1.Start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            client.Deinitialize();
+            client.Dispose();
+            initalized = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        bool n = false;
+        private void function1()
+        {
+            GetProcID(0);
+            int procId = -1;
+            var processes = from p in Process.GetProcessesByName("notepad++")
+                            select p;
+
+            foreach (var process in processes)
+            {
+                string titlee = process.MainWindowTitle;
+                string resultado = titlee.Substring(titlee.LastIndexOf(@"\") + 1);
+
+                string[] resultado2 = resultado.Split('-');
+
+                if(n == false)
+                {
+                    phpact = resultado2[0];
+                    n = true;
+                    break;
+                }
+                else
+                {
+                    if (phpact == resultado2[0])
+                    {
+
+                    }
+                    else
+                    {
+                        phpact = resultado2[0];
+                        update1();
+                    }
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            function1();
+        }
+        public void update()
+        {
+            if (initalized == false)
+            {
+                MessageBox.Show("É preciso inicializar primeiro a aplicação!");
+            }
+            else
+            {
+                function1();
+                client.SetPresence(new DiscordRPC.RichPresence()
+                {
+                    Details = $"{phpact}",
+                    State = $"{textBox1.Text}",
+                    Timestamps = Timestamps.Now,
+                    Assets = new Assets()
+                    {
+                        LargeImageKey = $"{textBox4.Text}",
+                        LargeImageText = "PHP",
+                        SmallImageKey = $"{textBox5.Text}",
+                        SmallImageText = "Visual Studio Code"
+                    }
+                });
+            }
+        }
+
+        public void update1()
+        {
+            if (procPID == "0")
+            {
+                if (initalized == false)
+                {
+                    MessageBox.Show("É preciso inicializar primeiro a aplicação!");
+                }
+                else
+                {
+                    function1();
+                    client.SetPresence(new DiscordRPC.RichPresence()
+                    {
+                        Details = $"N/A",
+                        State = $"{textBox1.Text}",
+                        Timestamps = Timestamps.Now,
+                        Assets = new Assets()
+                        {
+                            LargeImageKey = $"{textBox4.Text}",
+                            LargeImageText = "PHP",
+                            SmallImageKey = $"{textBox5.Text}",
+                            SmallImageText = "Visual Studio Code"
+                        }
+                    });
+                }
+            }
+            else
+            {
+                if (initalized == false)
+                {
+                    MessageBox.Show("É preciso inicializar primeiro a aplicação!");
+                }
+                else
+                {
+                    function1();
+                    client.SetPresence(new DiscordRPC.RichPresence()
+                    {
+                        Details = $"{phpact}",
+                        State = $"{textBox1.Text}",
+                        Timestamps = Timestamps.Now,
+                        Assets = new Assets()
+                        {
+                            LargeImageKey = $"{textBox4.Text}",
+                            LargeImageText = "PHP",
+                            SmallImageKey = $"{textBox5.Text}",
+                            SmallImageText = "Visual Studio Code"
+                        }
+                    });
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            function1();
+        }
+    }
+}
